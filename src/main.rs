44456dd2a0b302c010203fd6ee0cc1fe08369627
cinderowo <charlesohassellharris@gmail.com>
2023-12-10 19:07:10 +1000

    Added GUI, proper ending.

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,18 @@ const PADDLE_SPEED: f32 = 300.0;
 
 const BALL_SIZE: f32 = 32.0;
 
+enum GameState {
+    Menu,
+    Game,
+    GameOver
+}
+
+#[derive(Clone)]
+enum GuiButtonTypes {
+    Play,
+    Quit
+}
+
 enum PaddleType {
     Player,
     Enemy,
@@ -33,15 +45,26 @@ struct Ball {
     y_vel: f32,
 }
 
+#[derive(Clone)]
+struct GuiButton {
+    x_pos: f32,
+    y_pos: f32,
+    x_size: f32,
+    y_size: f32,
+    text: String,
+    result: GuiButtonTypes
+}
+
 #[derive(AppState)]
 struct Game {
     font: Font,
-    last_key: Option<KeyCode>,
     elapsed: f32,
     rng: ThreadRng,
+    state: GameState,
     player: Paddle,
     enemy: Paddle,
     ball: Ball,
+    gui: Vec<GuiButton>,
 }
 
 
@@ -65,9 +88,10 @@ fn setup(gfx: &mut Graphics) -> Game {
     
     let rng = rand::thread_rng();
 
+    let mut state = GameState::Menu;
+
     Game {
         font, 
-        last_key: None,
         player: Paddle {
             x_pos: PADDLE_DIST_FROM_EDGE,
             y_pos: (SCREEN_HEIGHT / 2.0) - (PADDLE_HEIGHT / 2.0),
@@ -75,6 +99,7 @@ fn setup(gfx: &mut Graphics) -> Game {
         },
         elapsed: 0.0,
         rng,
+        state,
         enemy: Paddle { 
             x_pos: SCREEN_WIDTH - PADDLE_DIST_FROM_EDGE - PADDLE_WIDTH, 
             y_pos: (SCREEN_HEIGHT / 2.0) - (PADDLE_HEIGHT / 2.0), 
@@ -83,13 +108,65 @@ fn setup(gfx: &mut Graphics) -> Game {
         ball: Ball { 
             x_pos: (SCREEN_WIDTH / 2.0) - (BALL_SIZE / 2.0), 
             y_pos: (SCREEN_HEIGHT / 2.0) - (BALL_SIZE / 2.0), 
-            x_vel: -100.0, 
+            x_vel: -200.0,
             y_vel: -1.0, 
-        }
+        },
+        gui: vec![
+            GuiButton {
+                x_pos: SCREEN_WIDTH / 3.0,
+                y_pos: 199.0,
+                x_size: SCREEN_WIDTH / 3.0,
+                y_size: 100.0,
+                text: "Play".to_string(),
+                result: GuiButtonTypes::Play
+            },
+            GuiButton {
+                x_pos: SCREEN_WIDTH / 3.0,
+                y_pos: 349.0,
+                x_size: SCREEN_WIDTH / 3.0,
+                y_size: 100.0,
+                text: "Quit".to_string(),
+                result: GuiButtonTypes::Quit
+            }
+        ]
     }
 }
 
 fn update(app: &mut App, state: &mut Game) {
+    match state.state {
+        GameState::Menu => menu_update(app, state),
+        GameState::Game => game_update(app, state),
+        GameState::GameOver => game_over_update(app, state)
+    }
+
+}
+
+fn menu_update(app: &mut App, state: &mut Game) {
+    if app.keyboard.was_pressed(KeyCode::Escape) {
+        println!("Exiting game TwT");
+        app.exit();
+    } 
+
+    let (m_x, m_y) = app.mouse.position();
+
+    if app.mouse.left_was_released() {
+        for element in state.gui.clone() {
+            if element.x_pos <= m_x && m_x <= element.x_pos + element.x_size &&
+            element.y_pos <= m_y && m_y <= element.y_pos + element.y_size 
+            {
+                match element.result {
+                    GuiButtonTypes::Play => state.state = GameState::Game,
+                    GuiButtonTypes::Quit => {
+                        println!("Exiting game TwT");
+                        app.exit()
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn game_update(app: &mut App, state: &mut Game) {
     state.elapsed = app.system_timer.delta().as_secs_f32();
 
     if app.keyboard.was_pressed(KeyCode::Escape) {
@@ -124,11 +201,56 @@ fn update(app: &mut App, state: &mut Game) {
         state.ball.y_vel *= -1.0;
     }
 
+    if (state.ball.x_pos < 0.0) || (state.ball.x_pos + BALL_SIZE > SCREEN_WIDTH) {
+        state.state = GameState::GameOver;
+    }
+
     state.ball.x_pos = state.ball.x_pos + state.ball.x_vel * state.elapsed;
     state.ball.y_pos = state.ball.y_pos + state.ball.y_vel * state.elapsed;
 }
 
+fn game_over_update(app: &mut App, _state: &mut Game) {
+    if app.keyboard.was_pressed(KeyCode::Escape) {
+        println!("Exiting game TwT");
+        app.exit();
+    } 
+}
+
 fn draw(gfx: &mut Graphics, state: &mut Game) {
+    match state.state {
+        GameState::Menu => menu_draw(gfx, state),
+        GameState::Game => game_draw(gfx, state),
+        GameState::GameOver => game_over_draw(gfx, state)
+    }
+}
+
+fn menu_draw(gfx: &mut Graphics, state: &mut Game) {
+    let mut draw = gfx.create_draw();
+    draw.clear(Color::BLACK);
+
+    for element in state.gui.clone() {
+        draw.rect((element.x_pos, element.y_pos), (element.x_size, element.y_size))
+            .color(Color::ORANGE);
+
+        draw.text(&state.font, &element.text)
+            .position(element.x_pos + element.x_size / 2.0, element.y_pos + element.y_size / 10.0)
+            .size(element.y_size * 0.8)
+            .color(Color::BLACK)
+            .h_align_center();
+    }
+
+    draw.text(&state.font, "Pong TWOOOOOO™")
+        .position(SCREEN_WIDTH / 2.0, 50.0)
+        .size(99.0)
+        .color(Color::ORANGE)
+        .h_align_center();
+
+
+    gfx.render(&draw);
+
+}
+
+fn game_draw(gfx: &mut Graphics, state: &mut Game) {
     let mut draw = gfx.create_draw();
     draw.clear(Color::BLACK);
 
@@ -139,9 +261,22 @@ fn draw(gfx: &mut Graphics, state: &mut Game) {
         .color(Color::WHITE);
 
     draw.rect((state.ball.x_pos, state.ball.y_pos), (BALL_SIZE, BALL_SIZE))
-        .color(Color::WHITE);
+        .color(Color::MAGENTA);
 
     gfx.render(&draw);
+}
+
+fn game_over_draw(gfx: &mut Graphics, state: &mut Game) {
+    let mut draw = gfx.create_draw();
+    draw.clear(Color::BLACK);
+
+    draw.text(&state.font, "You have died.")
+        .position(SCREEN_WIDTH / 2.0, 250.0)
+        .size(200.0)
+        .color(Color::MAROON)
+        .h_align_center();
+
+    gfx.render(&draw)
 }
 
 #[notan_main]
